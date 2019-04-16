8ba6bc2ea06bfe42af4797f3cbbde4e701f24d80
JulioVanzelli <julio.cesar@justdigital.com.br>
2019-04-15 23:53:49 -0300

    Init docker project

---
+++ spring/src/main/java/br/com/fiap/demofiap/DemoFiapApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.demofiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFiapApplication.class, args);
+	}
+
+}
